9bf93c3c40203c0f92fb6ea66ee944d60a55d8c2
Vladimir Yudintsev <yv87@ya.ru>
2014-07-24 14:56:14 +0600

    login

--- question_answer/q_a_t/forms.py
+++ question_answer/q_a_t/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, AuthenticationForm)
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Submit
@@ -15,5 +16,19 @@ class RegistrationForm(UserCreationForm):
             'password2',
             ButtonHolder(
                 Submit('register', 'Register', css_class='btn-primary')
+            ),
+        )
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            ButtonHolder(
+                Submit('login', 'Login', css_class='btn-primary')
             ),
         )
--- question_answer/q_a_t/urls.py
+++ question_answer/q_a_t/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from .views import HomePageView, SignUpView
+from .views import HomePageView, SignUpView, LoginView
 
 
 urlpatterns = patterns('',
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
     url(r'^home$', HomePageView.as_view(), name='home'),
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^accounts/register/$', SignUpView.as_view(), name='signup'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', SignUpView.as_view(), name='logout'),
 )
