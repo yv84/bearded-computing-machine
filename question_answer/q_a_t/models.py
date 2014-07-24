ba8b2e75e560fd68ab85ca2a93db935e04c4488c
Vladimir Yudintsev <yv87@ya.ru>
2014-07-24 14:15:08 +0600

    create home page

--- question_answer/q_a_t/models.py
+++ question_answer/q_a_t/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+# model answer:
+#     text varchar
+#     y/n boolean
+#
+# model ask:
+#     test varchar
+#     foreignkey answer1
+#     foreignkey answer2
+#     foreignkey answer3
+#     foreignkey answer4
+#     foreignkey answer5
+#
+# model bilet:
+#     nomer integer
+#     foreignkey ask1
+#     foreignkey ask2
+#     foreignkey ask3
+#     foreignkey ask4
+#     foreignkey ask5
+#     foreignkey ask6
+#     foreignkey ask7
+#     foreignkey ask8
+#     foreignkey ask9
--- question_answer/q_a_t/urls.py
+++ question_answer/q_a_t/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+from .views import HomePageView
 
-import q_a_t
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'question_answer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('q_a_t.urls')),
+    url(r'^home$', HomePageView.as_view(), name='home')
 )
